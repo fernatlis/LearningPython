505377bfc763dec14bc6d8b915e5c2aec8d7c682
fernandogarcia <fernandogarcia28>
2015-10-31 17:37:42 +1000

    My first navigation using flask

--- Main.py
+++ Main.py
@@ -1,19 +1,26 @@
-from flask import Flask, render_template, json, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def main():
+    print("inside main")
     return render_template('index.html')
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/main")
+def home():
+    return render_template('index.html')
 
 
 @app.route('/signup')
 def signup():
+    print("inside signup")
     return render_template('signup.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
 
 # Point your browser to http://localhost:5000/ and you should have the welcome message.
